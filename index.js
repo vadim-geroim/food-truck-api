3c5d0545573d09da9d149b665a0dfc35452789de
Vadim Geroim <vgeroim.qa@gmail.com>
2024-09-02 13:19:36 -0400

    Add endpoint to remove food truck

--- index.js
+++ index.js
@@ -32,24 +32,36 @@ app.get("/food-trucks/:locationid", (req, res) => {
 
 //Update an existing food truck
 app.put("/food-trucks/:locationid", (req, res) => {
-    const locationid = parseInt(req.params.locationid);
-    const index = foodTrucks.data.findIndex((t) => locationid === t.locationid);
-  
-    if (index !== -1) {
-      const updatedTruck = { ...foodTrucks.data[index], ...req.body };
-      foodTrucks.data[index] = updatedTruck; //The data is stored in-memory, just for the demonstration purpose
-      res.json(updatedTruck);
-    } else {
-      res.status(404).send("Food truck not found");
-    }
-  });
+  const locationid = parseInt(req.params.locationid);
+  const index = foodTrucks.data.findIndex((t) => locationid === t.locationid);
+
+  if (index !== -1) {
+    const updatedTruck = { ...foodTrucks.data[index], ...req.body };
+    foodTrucks.data[index] = updatedTruck; //The data is stored in-memory, just for the demonstration purpose
+    res.json(updatedTruck);
+  } else {
+    res.status(404).send("Food truck not found");
+  }
+});
 
 //Add a new food truck
 app.post("/food-trucks", (req, res) => {
-    const newTruck = req.body;
-    foodTrucks.data.push(newTruck); //The data is stored in memory, just for the demonstration purpose.
-    res.status(201).json(newTruck);
-  });
+  const newTruck = req.body;
+  foodTrucks.data.push(newTruck); //The data is stored in memory, just for the demonstration purpose.
+  res.status(201).json(newTruck);
+});
+
+//Remove food truck
+app.delete("/food-trucks/:locationid", (req, res) => {
+  const locationid = parseInt(req.params.locationid);
+  const index = foodTrucks.data.findIndex((t) => locationid === t.locationid);
+  if (index !== -1) {
+    foodTrucks.data.splice(index, 1); //The data is deleted from memory, just for the demonstration purpose.
+    res.send(`Food truck with locationid ${locationid} has been deleted`);
+  } else {
+    res.status(404).send("Food truck not found");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on http://localhost:${port}`);
