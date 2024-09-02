8665f50709298b236c33c8e914930d28702833ef
Vadim Geroim <vgeroim.qa@gmail.com>
2024-09-02 14:52:31 -0400

    Add validation and error handling for each endpoint

---
+++ helpers/errorHandler.js
@@ -0,0 +1,11 @@
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+
+  res.status(err.status || 500).json({
+    error: {
+      message: err.message || "Internal Server Error",
+    },
+  });
+};
+
+module.exports = errorHandler;
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { body, param, validationResult } = require("express-validator");
+const errorHandler = require("./helpers/errorHandler");
 const app = express();
 const port = 3000;
 
@@ -58,16 +60,26 @@ app.get("/food-trucks", (req, res) => {
  */
 
 //Get a food truck by locationid
-app.get("/food-trucks/:locationid", (req, res) => {
-  const locationid = parseInt(req.params.locationid);
-  const truck = foodTrucks.data.find((t) => t.locationid === locationid);
+app.get(
+  "/food-trucks/:locationid",
+  [param("locationid").isInt().withMessage("Location ID must be an integer")],
+  (req, res) => {
+    const errors = validationResult(req);
 
-  if (truck) {
-    res.json(truck);
-  } else {
-    res.json(404).send("Food truck not found");
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const locationid = parseInt(req.params.locationid);
+    const truck = foodTrucks.data.find((t) => t.locationid === locationid);
+
+    if (truck) {
+      res.json(truck);
+    } else {
+      res.status(404).json({ error: "Food truck not found" });
+    }
   }
-});
+);
 
 /**
  * @swagger
@@ -95,18 +107,37 @@ app.get("/food-trucks/:locationid", (req, res) => {
  */
 
 //Update an existing food truck
-app.put("/food-trucks/:locationid", (req, res) => {
-  const locationid = parseInt(req.params.locationid);
-  const index = foodTrucks.data.findIndex((t) => locationid === t.locationid);
+app.put(
+  "/food-trucks/:locationid",
+  [
+    param("locationid").isInt().withMessage("Location ID must be an integer"),
+    body("Applicant")
+      .optional()
+      .notEmpty()
+      .withMessage("Applicant name is required"),
+    body("FacilityType")
+      .optional()
+      .notEmpty()
+      .withMessage("Facility type is required"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-  if (index !== -1) {
-    const updatedTruck = { ...foodTrucks.data[index], ...req.body };
-    foodTrucks.data[index] = updatedTruck; //The data is stored in-memory, just for the demonstration purpose
-    res.json(updatedTruck);
-  } else {
-    res.status(404).send("Food truck not found");
+    const locationid = parseInt(req.params.locationid);
+    const index = foodTrucks.data.findIndex((t) => locationid === t.locationid);
+
+    if (index !== -1) {
+      const updatedTruck = { ...foodTrucks.data[index], ...req.body };
+      foodTrucks.data[index] = updatedTruck; //The data is stored in-memory, just for the demonstration purpose
+      res.json(updatedTruck);
+    } else {
+      res.status(404).json({ error: "Food truck not found" });
+    }
   }
-});
+);
 
 /**
  * @swagger
@@ -127,11 +158,24 @@ app.put("/food-trucks/:locationid", (req, res) => {
  */
 
 //Add a new food truck
-app.post("/food-trucks", (req, res) => {
-  const newTruck = req.body;
-  foodTrucks.data.push(newTruck); //The data is stored in memory, just for the demonstration purpose.
-  res.status(201).json(newTruck);
-});
+app.post(
+  "/food-trucks",
+  [
+    body("locationid").isInt().withMessage("Location ID must be an integer"),
+    body("Applicant").notEmpty().withMessage("Applicant name is required"),
+    body("FacilityType").notEmpty().withMessage("Facility type is required"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const newTruck = req.body;
+    foodTrucks.data.push(newTruck); //The data is stored in memory, just for the demonstration purpose.
+    res.status(201).json(newTruck);
+  }
+);
 
 /**
  * @swagger
@@ -153,36 +197,46 @@ app.post("/food-trucks", (req, res) => {
  */
 
 //Remove food truck
-app.delete("/food-trucks/:locationid", (req, res) => {
-  const locationid = parseInt(req.params.locationid);
-  const index = foodTrucks.data.findIndex((t) => locationid === t.locationid);
-  if (index !== -1) {
-    foodTrucks.data.splice(index, 1); //The data is deleted from memory, just for the demonstration purpose.
-    res.send(`Food truck with locationid ${locationid} has been deleted`);
-  } else {
-    res.status(404).send("Food truck not found");
+app.delete(
+  "/food-trucks/:locationid",
+  [param("locationid").isInt().withMessage("Location ID must be an integer")],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const locationid = parseInt(req.params.locationid);
+    const index = foodTrucks.data.findIndex((t) => locationid === t.locationid);
+    if (index !== -1) {
+      foodTrucks.data.splice(index, 1); //The data is deleted from memory, just for the demonstration purpose.
+      res.send(`Food truck with locationid ${locationid} has been deleted`);
+    } else {
+      res.status(404).json({ error: "Food truck not found" });
+    }
   }
-});
+);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is listening on http://localhost:${port}`);
 });
 
-
-const swaggerUI = require('swagger-ui-express');
-const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 
 const swaggerOptions = {
-    swaggerDefinition: {
-        openapi: '3.0.0',
-        info: {
-          title: 'Food Truck API',
-          version: '1.0.0',
-          description: 'API for managing food trucks',
-        },
-      },
-      apis: ['./index.js']
-}
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Food Truck API",
+      version: "1.0.0",
+      description: "API for managing food trucks",
+    },
+  },
+  apis: ["./index.js"],
+};
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
