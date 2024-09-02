965e07956174ab25505d46e216f601e56a7215bb
Vadim Geroim <vgeroim.qa@gmail.com>
2024-09-02 13:11:38 -0400

    Add endpoints get all food trucks and get food truck by id

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
 //Import mock data
-const foodTrucks = require('./data/trucks-data.json');
+const foodTrucks = require("./data/trucks-data.json");
 
 //Parse JSON
 app.use(express.json());
 
 //Check if server is running
-app.get('/', (req, res) => {
-    res.send('Food Truck API is running');
+app.get("/", (req, res) => {
+  res.send("Food Truck API is running");
+});
+
+//Get all food trucks
+app.get("/food-trucks", (req, res) => {
+  res.json(foodTrucks.data);
+});
+
+app.get("/food-trucks/:locationid", (req, res) => {
+  const locationid = parseInt(req.params.locationid);
+  const truck = foodTrucks.data.find((t) => t.locationid === locationid);
+
+  if (truck) {
+    res.json(truck);
+  } else {
+    res.json(404).send("Food truck not found");
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server is listening on http://localhost:${port}`);
+  console.log(`Server is listening on http://localhost:${port}`);
 });
